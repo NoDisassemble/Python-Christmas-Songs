07d2c763e4b7f6bbd6695412f700242fd2237661
NoDisassemble <anything@dant35an0nym0u5.com>
2016-12-18 00:42:28 -0800

    Add files via upload
    Jingle Bells
    Rudolph The Red Nosed Reindeer

---
+++ christmasSongs.py
@@ -0,0 +1,420 @@
+import winsound
+import sys
+import time
+
+print('''
+  .-._   .-._.                                                     .-.
+..' (_)`-'   /         .-.       /                            .--.-'
+|          /-.   ).--.`-' . ---/---.  .-. .-.  .-.    .     (  (_).-._..  .-.    .-.    .
+|    _    /   | /    /   / \  /     )/   )   )(  |   / \     `-. (   )  )/   )  (   )  / \\
+`.    )_.'    |/  _.(__./ ._)/     '/   /   (  `-'-'/ ._)  _    ) `-'  '/   (    `-/-'/ ._)
+  `--'                 /                     `-'   /      (_.--'             `--._/  /
+
+--------------------------------------------------------------------------------------------
+2016 v1.0
+by
+NoDisassemble.me
+--------------------------------------------------------------------------------------------
+''')
+
+while True:
+    # Begin Jingle Bells
+    def opt_a():
+        loading = "Generating Song and Lyrics: [-------------------]"
+        print("")
+        for i in range(101):
+            time.sleep(0.05)
+            sys.stdout.write("\r" + loading + " %d%%" % i)
+            if i == 5 or i == 10 or i == 15 or i == 20 or i == 25 or i == 30 or i == 35 or i == 40 or i == 45 or i == 50 or i == 55 or i == 60 or i == 65 or i == 70 or i == 75 or i == 80 or i == 85 or i == 90 or i == 95 or i == 100:
+                loading = loading.replace("-", "=", 1)
+            sys.stdout.flush()
+        print("")
+        print("")
+        x = "-"
+        for i in range(1, 28):
+            print(x, end="")
+        print("")
+        print("[+] Plying Jingle Bells [+]")
+        x = "-"
+        for i in range(1, 28):
+            print(x, end="")
+        print("")
+        time.sleep(1)
+        print("Jingle")
+        winsound.Beep(660, 500)   #E
+        winsound.Beep(660, 500)   #E
+        print("Bells")
+        winsound.Beep(660, 1000)  #E
+        print("Jingle")
+        winsound.Beep(660, 500)   #E
+        winsound.Beep(660, 500)   #E
+        print("Bells")
+        winsound.Beep(660, 1000)  #E
+        print("Jingle")
+        winsound.Beep(660, 500)   #E
+        winsound.Beep(784, 500)   #G
+        print("All")
+        winsound.Beep(523, 700)   #C
+        print("The")
+        winsound.Beep(587, 300)   #D
+        print("Waaaaaaay")
+        winsound.Beep(660, 1200)  #E
+        time.sleep(1)
+        print("Oh")
+        winsound.Beep(700, 500)   #F
+        print("What")
+        winsound.Beep(700, 500)   #F
+        print("Fun")
+        winsound.Beep(700, 1000)  #F
+        print("It")
+        winsound.Beep(700, 300)   #F
+        print("Is")
+        winsound.Beep(700, 500)   #F
+        print("To")
+        winsound.Beep(660, 500)   #E
+        print("Ride")
+        winsound.Beep(660, 700)   #E
+        print("On")
+        winsound.Beep(660, 300)   #E
+        print("A")
+        winsound.Beep(660, 300)   #E
+        print("One")
+        winsound.Beep(660, 300)   #E
+        print("Horse")
+        winsound.Beep(587, 500)   #D
+        print("Open")
+        winsound.Beep(587, 500)   #D
+        winsound.Beep(660, 500)   #E
+        print("Sleigh")
+        winsound.Beep(587, 1000)  #D
+        print("HEEEEEY!")
+        winsound.Beep(784, 1000)  #G
+        print("")
+        time.sleep(0.2)
+        print("Jingle")
+        winsound.Beep(660, 500)   #E
+        winsound.Beep(660, 500)   #E
+        print("Bells")
+        winsound.Beep(660, 1000)  #E
+        print("Jingle")
+        winsound.Beep(660, 500)   #E
+        winsound.Beep(660, 500)   #E
+        print("Bells")
+        winsound.Beep(660, 1000)  #E
+        print("Jingle")
+        winsound.Beep(660, 500)   #E
+        winsound.Beep(784, 500)   #G
+        print("All")
+        winsound.Beep(523, 700)   #C
+        print("The")
+        winsound.Beep(587, 300)   #D
+        print("Waaaaaaay")
+        winsound.Beep(660, 1200)  #E
+        time.sleep(1)
+        print("Oh")
+        winsound.Beep(700, 500)   #F
+        print("What")
+        winsound.Beep(700, 500)   #F
+        print("Fun")
+        winsound.Beep(700, 1000)  #F
+        print("It")
+        winsound.Beep(700, 300)   #F
+        print("Is")
+        winsound.Beep(700, 500)   #F
+        print("To")
+        winsound.Beep(660, 500)   #E
+        print("Ride")
+        winsound.Beep(660, 700)   #E
+        print("On")
+        winsound.Beep(660, 300)   #E
+        print("A")
+        winsound.Beep(660, 300)   #E
+        print("OOOOOOOONE")
+        winsound.Beep(784, 1000)  #G
+        print("HOOOOOORSE")
+        winsound.Beep(784, 1000)  #G
+        print("OOOOOOOPEN")
+        winsound.Beep(880, 1000)  #A
+        winsound.Beep(988, 1000)  #B
+        print("SLLLLLLEIGH!!!")
+        winsound.Beep(1046, 2500) #C
+        time.sleep(0.5)
+        winsound.Beep(523, 500)   #C
+        time.sleep(1)
+        x = "-"
+        for i in range(1, 28):
+            print(x, end="")
+        print("")
+        print("[+] Song Completed [+]")
+        x = "-"
+        for i in range(1, 28):
+            print(x, end="")
+        print("")
+        print("")
+        input("Press Enter to play another song: ")
+        print("")
+    # Begin Rudolph
+    def opt_b():
+        loading = "Generating Song and Lyrics: [-------------------]"
+        print("")
+        for i in range(101):
+            time.sleep(0.05)
+            sys.stdout.write("\r" + loading + " %d%%" % i)
+            if i == 5 or i == 10 or i == 15 or i == 20 or i == 25 or i == 30 or i == 35 or i == 40 or i == 45 or i == 50 or i == 55 or i == 60 or i == 65 or i == 70 or i == 75 or i == 80 or i == 85 or i == 90 or i == 95 or i == 100:
+                loading = loading.replace("-", "=", 1)
+            sys.stdout.flush()
+        print("")
+        print("")
+        x = "-"
+        for i in range(1, 46):
+            print(x, end="")
+        print("")
+        print("[+] Plying Rudolph The Red Nosed Reindeer [+]")
+        x = "-"
+        for i in range(1, 46):
+            print(x, end="")
+        print("")
+        time.sleep(1)
+        print("Rudolph")
+        winsound.Beep(588, 500)  #D
+        winsound.Beep(660, 500)  #E
+        print("The")
+        winsound.Beep(588, 300)  #D
+        print("Red")
+        winsound.Beep(494, 500)  #B
+        print("Nosed")
+        winsound.Beep(784, 700)  #G
+        print("Reindeer")
+        winsound.Beep(660, 500)  #E
+        winsound.Beep(588, 1000) #D
+        time.sleep(0.5)
+        print("Had")
+        winsound.Beep(588, 300)  #D
+        print("A")
+        winsound.Beep(660, 300)  #E
+        print("Very")
+        winsound.Beep(588, 300)  #D
+        winsound.Beep(660, 300)  #E
+        print("Shiny")
+        winsound.Beep(588, 700)  #D
+        winsound.Beep(784, 700)  #G
+        print("Nose")
+        winsound.Beep(740, 1200) #F#
+        time.sleep(1)
+        print("And")
+        winsound.Beep(522, 500)  #C
+        print("If")
+        winsound.Beep(588, 500)  #D
+        print("You")
+        winsound.Beep(522, 300)  #C
+        print("Ever")
+        winsound.Beep(440, 500)  #A
+        winsound.Beep(784, 700)  #F#
+        print("Saw")
+        winsound.Beep(660, 500)  #E
+        print("It")
+        winsound.Beep(588, 1000) #D
+        time.sleep(0.5)
+        print("You")
+        winsound.Beep(588, 300)  #D
+        print("Would")
+        winsound.Beep(660, 300)  #E
+        print("Even")
+        winsound.Beep(588, 300)  #D
+        winsound.Beep(660, 300)  #E
+        print("Say")
+        winsound.Beep(588, 700)  #D
+        print("It")
+        winsound.Beep(660, 700)  #
+        print("Glows")
+        winsound.Beep(494, 1200) #B
+        time.sleep(1)
+        print("All")
+        winsound.Beep(588, 500)  #D
+        print("Of")
+        winsound.Beep(660, 500)  #E
+        print("The")
+        winsound.Beep(588, 300)  #D
+        print("Other")
+        winsound.Beep(494, 500)  #B
+        winsound.Beep(784, 700)  #G
+        print("Reindeer")
+        winsound.Beep(660, 500)  #E
+        winsound.Beep(588, 1000) #D
+        time.sleep(0.5)
+        print("Used")
+        winsound.Beep(588, 300)  #D
+        print("To")
+        winsound.Beep(660, 300)  #E
+        print("Laugh")
+        winsound.Beep(588, 300)  #D
+        print("And")
+        winsound.Beep(660, 300)  #E
+        print("Call")
+        winsound.Beep(588, 700)  #D
+        print("Him")
+        winsound.Beep(784, 700)  #G
+        print("Names")
+        winsound.Beep(740, 1200) #F#
+        time.sleep(1)
+        print("They")
+        winsound.Beep(522, 500)  #C
+        print("Never")
+        winsound.Beep(588, 500)  #D
+        winsound.Beep(522, 300)  #C
+        print("Let")
+        winsound.Beep(440, 500)  #A
+        print("Poor")
+        winsound.Beep(784, 700)  #F#
+        print("Rudolph")
+        winsound.Beep(660, 500)  #E
+        winsound.Beep(588, 1000) #D
+        time.sleep(0.5)
+        print("Join")
+        winsound.Beep(588, 300)  #D
+        print("In")
+        winsound.Beep(660, 300)  #E
+        print("Any")
+        winsound.Beep(588, 300)  #D
+        winsound.Beep(660, 300)  #E
+        print("Reindeer")
+        winsound.Beep(588, 700)  #D
+        winsound.Beep(440, 700)  #A
+        print("Games")
+        winsound.Beep(392, 1200) #G
+        time.sleep(1)
+        print("Then")
+        winsound.Beep(660, 500)  #E
+        print("One")
+        winsound.Beep(660, 500)  #E
+        print("Foggy")
+        winsound.Beep(784, 500)  #G
+        winsound.Beep(660, 500)  #E
+        print("Christmas")
+        winsound.Beep(588, 700)  #D
+        winsound.Beep(494, 500)  #B
+        print("Eve")
+        winsound.Beep(588, 1000) #D
+        time.sleep(0.5)
+        print("Santa")
+        winsound.Beep(522, 500)  #C
+        winsound.Beep(660, 500)  #E
+        print("Came")
+        winsound.Beep(588, 500)  #D
+        print("To")
+        winsound.Beep(522, 500)  #C
+        print("Say")
+        winsound.Beep(494, 1200) #B
+        time.sleep(1)
+        print("Rudolph")
+        winsound.Beep(440, 500)  #A
+        winsound.Beep(494, 500)  #B
+        print("With")
+        winsound.Beep(588, 500)  #D
+        print("Your")
+        winsound.Beep(660, 500)  #
+        print("Nose")
+        winsound.Beep(740, 500)  #F#
+        print("So")
+        winsound.Beep(740, 500)  #F#
+        print("Bright")
+        winsound.Beep(740, 1000) #F#
+        time.sleep(0.5)
+        print("Wont")
+        winsound.Beep(784, 500)  #G
+        print("You")
+        winsound.Beep(784, 500)  #G
+        print("Guide")
+        winsound.Beep(740, 500)  #F#
+        print("My")
+        winsound.Beep(660, 500)  #E
+        print("Sleigh")
+        winsound.Beep(588, 500)  #D
+        print("Tonight")
+        winsound.Beep(522, 500)  #C
+        winsound.Beep(440, 1000) #A
+        time.sleep(0.5)
+        print("Then")
+        winsound.Beep(588, 500)  #D
+        print("How")
+        winsound.Beep(660, 500)  #E
+        print("The")
+        winsound.Beep(588, 300)  #D
+        print("Reindeer")
+        winsound.Beep(494, 500)  #B
+        winsound.Beep(784, 700)  #G
+        print("Loved")
+        winsound.Beep(660, 500)  #E
+        print("Him")
+        winsound.Beep(588, 1000) #D
+        time.sleep(0.5)
+        print("As")
+        winsound.Beep(588, 300)  #D
+        print("They")
+        winsound.Beep(660, 300)  #E
+        print("Shouted")
+        winsound.Beep(588, 300)  #D
+        winsound.Beep(660, 300)  #E
+        print("Out")
+        winsound.Beep(588, 700)  #D
+        print("With")
+        winsound.Beep(784, 700)  #G
+        print("Glee")
+        winsound.Beep(740, 1200) #F#
+        time.sleep(1)
+        print("Rudolph")
+        winsound.Beep(522, 500)  #C
+        winsound.Beep(588, 500)  #D
+        print("The")
+        winsound.Beep(522, 300)  #C
+        print("Red")
+        winsound.Beep(440, 500)  #A
+        print("Nosed")
+        winsound.Beep(784, 700)  #F#
+        print("Reindeer")
+        winsound.Beep(660, 500)  #E
+        winsound.Beep(588, 1000) #D
+        time.sleep(0.5)
+        print("You'll")
+        winsound.Beep(588, 300)  #D
+        print("Go")
+        winsound.Beep(660, 300)  #E
+        print("Down")
+        winsound.Beep(588, 300)  #D
+        print("In")
+        winsound.Beep(660, 300)  #E
+        print("History!")
+        winsound.Beep(588, 1000) #D
+        winsound.Beep(880, 1000) #A
+        winsound.Beep(784, 3000) #G
+        time.sleep(1)
+        x = "-"
+        for i in range(1, 28):
+            print(x, end="")
+        print("")
+        print("[+] Song Completed [+]")
+        x = "-"
+        for i in range(1, 28):
+            print(x, end="")
+        print("")
+        print("")
+        input("Press Enter to play another song: ")
+        print("")
+    # Catch for invalid response
+    def invalid_opt():
+        print("")
+        print("Invalid choice")
+        input("Press [Enter] to try again:")
+        print("")
+    # Options of Songs to choose from
+    options = {"A":["Jingle Bells",opt_a], "B":["Rudolph The Red Nosed Reindeer",opt_b]}
+    for option in options:
+        print(option+") "+options.get(option)[0])
+    # User Input for Choice
+    choice = input("What song would you like to play? [A or B]: ")
+    val = options.get(choice)
+    if val is not None:
+        action = val[1]
+    else:
+        action = invalid_opt
+    action()
